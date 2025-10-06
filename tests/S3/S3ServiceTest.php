1e4818fa24451b41d072ed547fa6bd0d9c43fb8e
Yakov <jsvidersky@192.168.1.8>
2025-10-06 17:43:21 +0300

    added s3 test

---
+++ tests/S3/S3ServiceTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace StaticService\Tests\S3;
+
+use Aws\Result;
+use Aws\S3\S3Client;
+use Aws\S3\S3ClientInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use StaticService\Interface\S3\Connection;
+use StaticService\Interface\S3\S3ServiceInterface;
+use StaticService\S3\S3Service;
+
+class S3ServiceTest extends TestCase
+{
+    private const BUCKET = 'test';
+    private S3ServiceInterface $service;
+    private Connection&MockObject $connectionMock;
+    private S3ClientInterface&MockObject $clientMock;
+
+    protected function setUp(): void
+    {
+        $this->connectionMock = $this->createMock(Connection::class);
+        $this->clientMock = $this->createMock(S3Client::class);
+        $this->service = new S3Service(
+            connection: $this->connectionMock,
+            bucket: self::BUCKET,
+        );
+    }
+
+    public function testGet(): void
+    {
+        $this->connectionMock
+        ->expects($this->once())
+        ->method('getClient')
+        ->willReturn($this->clientMock);
+
+        $this->clientMock
+        ->expects($this->once())
+        ->method('__call')
+        ->with('getObject')
+        ->willReturn(new Result(['Body' => 'value']));
+
+        $result = $this->service->get('key');
+        $this->assertSame('value', $result);
+    }
+
+    public function testAdd(): void
+    {
+        $this->connectionMock
+        ->expects($this->once())
+        ->method('getClient')
+        ->willReturn($this->clientMock);
+
+        $this->clientMock
+        ->expects($this->once())
+        ->method('__call')
+        ->with('putObject')
+        ->willReturn(new Result());
+
+        $result = $this->service->add('path');
+        $this->assertNotEmpty($result);
+        $this->assertIsString($result);
+    }
+}
